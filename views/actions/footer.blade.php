92bba7657f6eb4f185b3688b45ecfdac41877e52
Ruben <contact@rubenmadila.com>
2018-12-19 16:01:10 +0000

    footer

--- views/actions/footer.blade.php
+++ views/actions/footer.blade.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Madila
- * Date: 19/12/2018
- * Time: 13:09
- */
+<script src="{{asset('/assets/js/mikado/mikado.js')}}"></script>
+<script src="{{asset('/assets/js/mikado/modules/onScrolling.js')}}"></script>
+<script src="{{asset('/assets/js/mikado/modules/Reveal.js')}}"></script>
+<script src="{{asset('/assets/js/mikado/modules/Elastic.js')}}"></script>
+<script>
+    var systemConfig = {
+        baseURL: '/mikado/'
+    };
+</script>
+<noscript id="deferred-styles">
+    <link data-name="geomanistregular" data-weight="400" href="{{asset('fonts/geomanist-regular-webfont.woff2')}}" as="font">
+    <link data-name="geomanistregular" data-weight="500" href="{{asset('fonts/geomanist-book-webfont.woff2')}}" as="font">
+    <link data-name="geomanistregular" data-weight="200" href="{{asset('fonts/geomanist-extralight-webfont.woff2')}}" as="font">
+</noscript>
