fae30684217b731871690e1473d2cd585c59d4bc
Ruben <contact@rubenmadila.com>
2018-12-19 16:31:53 +0000

    fonts

--- views/actions/footer.blade.php
+++ views/actions/footer.blade.php
@@ -8,7 +8,7 @@
     };
 </script>
 <noscript id="deferred-styles">
-    <link data-name="geomanistregular" data-weight="400" href="{{asset('/assets/fonts/geomanist-regular-webfont.woff2')}}" as="font">
-    <link data-name="geomanistregular" data-weight="500" href="{{asset('/assets/fonts/geomanist-book-webfont.woff2')}}" as="font">
-    <link data-name="geomanistregular" data-weight="200" href="{{asset('/assets/fonts/geomanist-extralight-webfont.woff2')}}" as="font">
+    <link data-name="geomanistregular" data-weight="400" href="{{asset('/assets/fonts/geomanist/geomanist-regular-webfont.woff2')}}" as="font">
+    <link data-name="geomanistregular" data-weight="500" href="{{asset('/assets/fonts/geomanist/geomanist-book-webfont.woff2')}}" as="font">
+    <link data-name="geomanistregular" data-weight="200" href="{{asset('/assets/fonts/geomanist/geomanist-extralight-webfont.woff2')}}" as="font">
 </noscript>
