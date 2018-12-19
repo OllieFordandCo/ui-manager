f2e07a0da365e3defa307f477ca69f5a80297a22
Ruben <contact@rubenmadila.com>
2018-12-19 16:34:15 +0000

    fonts

--- views/actions/fonts.blade.php
+++ views/actions/fonts.blade.php
@@ -1,4 +1,7 @@
 <!-- Webfonts via FontLoader -->
 <link data-name="geomanistregular" data-weight="600" href="{{asset('/assets/fonts/geomanist/geomanist-medium-webfont.woff2')}}" as="font">
 <link data-name="geomanistregular" data-weight="300" href="{{asset('/assets/fonts/geomanist/geomanist-light-webfont.woff2')}}" as="font">
+<link data-name="geomanistregular" data-weight="400" href="{{asset('/assets/fonts/geomanist/geomanist-regular-webfont.woff2')}}" as="font">
+<link data-name="geomanistregular" data-weight="500" href="{{asset('/assets/fonts/geomanist/geomanist-book-webfont.woff2')}}" as="font">
+<link data-name="geomanistregular" data-weight="200" href="{{asset('/assets/fonts/geomanist/geomanist-extralight-webfont.woff2')}}" as="font">
 <script src="{{asset('/assets/js/mikado/modules/fontLoader.js')}}"></script>
